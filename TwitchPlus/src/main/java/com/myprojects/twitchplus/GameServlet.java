90a6d366c2f4b6c6ec0f21486d14cc414428239d
TheOldEnglishSheepdog <58194727+TheOldEnglishSheepdog@users.noreply.github.com>
2022-08-11 21:12:58 -0700

    game servlet

--- TwitchPlus/src/main/java/com/myprojects/twitchplus/GameServlet.java
+++ TwitchPlus/src/main/java/com/myprojects/twitchplus/GameServlet.java
@@ -1,5 +1,9 @@
 package com.myprojects.twitchplus;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myprojects.twitchplus.external.TwitchClient;
+import com.myprojects.twitchplus.external.TwitchException;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -7,11 +11,24 @@ import java.io.IOException;
 
 @WebServlet(name = "GameServlet", urlPatterns = {"/game"})
 public class GameServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //get gamename from request url
+        String gameName = request.getParameter("game_name");
+        TwitchClient client = new TwitchClient();
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello World");
+        //let the client know the returned data is in Json format
+        response.setContentType("application/json;charset=UTF-8");
+
+        try {
+            if (gameName != null) {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.searchGame(gameName)));
+            } else {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.topGames(0)));
+            }
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
     }
 }
 
