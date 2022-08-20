7c59c38acdf2ebab7ab48d673ad4c51e3e8e4c0f
TheOldEnglishSheepdog <58194727+TheOldEnglishSheepdog@users.noreply.github.com>
2022-08-20 14:22:40 -0700

    Recommendation functions

--- TwitchPlus/src/main/java/com/myprojects/twitchplus/servelt/FavoriteServlet.java
+++ TwitchPlus/src/main/java/com/myprojects/twitchplus/servelt/FavoriteServlet.java
@@ -17,10 +17,19 @@ import java.util.Map;
 public class FavoriteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Get user ID from request URL, this is a temporary solution since we don’t support session now
-        String userId = request.getParameter("user_id");
+        // Check if the session is still valid, which means the user has been logged in successfully.
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+        String userId = (String) session.getAttribute("user_id");
+
+        //String userId = request.getParameter("user_id");
         // Get favorite item information from request body
-        ObjectMapper mapper = new ObjectMapper();
-        FavoriteRequestBody body = mapper.readValue(request.getReader(), FavoriteRequestBody.class);
+        //ObjectMapper mapper = new ObjectMapper();
+        //FavoriteRequestBody body = mapper.readValue(request.getReader(), FavoriteRequestBody.class);
+        FavoriteRequestBody body = ServletUtil.readRequestBody(FavoriteRequestBody.class, request);
         if (body == null) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
@@ -42,15 +51,24 @@ public class FavoriteServlet extends HttpServlet {
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String userId = request.getParameter("user_id");
+        // Check if the session is still valid, which means the user has been logged in successfully.
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+        String userId = (String) session.getAttribute("user_id");
+
+        //String userId = request.getParameter("user_id");
         Map<String, List<Item>> itemMap;
         MySQLConnection connection = null;
         try {
             // Read the favorite items from the database
             connection = new MySQLConnection();
             itemMap = connection.getFavoriteItems(userId);
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().print(new ObjectMapper().writeValueAsString(itemMap));
+//            response.setContentType("application/json;charset=UTF-8");
+//            response.getWriter().print(new ObjectMapper().writeValueAsString(itemMap));
+            ServletUtil.writeItemMap(response, itemMap);
         } catch (MySQLException e) {
             throw new ServletException(e);
         } finally {
@@ -61,9 +79,16 @@ public class FavoriteServlet extends HttpServlet {
     }
 
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String userId = request.getParameter("user_id");
-        ObjectMapper mapper = new ObjectMapper();
-        FavoriteRequestBody body = mapper.readValue(request.getReader(), FavoriteRequestBody.class);
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+        String userId = (String) session.getAttribute("user_id");
+        //String userId = request.getParameter("user_id");
+        //ObjectMapper mapper = new ObjectMapper();
+        //FavoriteRequestBody body = mapper.readValue(request.getReader(), FavoriteRequestBody.class);
+        FavoriteRequestBody body = ServletUtil.readRequestBody(FavoriteRequestBody.class, request);
         if (body == null) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
